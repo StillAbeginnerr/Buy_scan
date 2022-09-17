0903f22ca290819a79e52dee43564d8b66042f36
shivam agarwal <shivamagarwaloff@gmail.com>
2022-09-17 15:29:38 +0530

    s

--- src/homepage.js
+++ src/homepage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./homepage.css"
+import qr from './QR-1.jpeg'
 
 
 
@@ -9,6 +10,7 @@ function Homepage() {
   return (
     <>
     <h2 className='title'> Scan And Pay</h2>
+<img className='qr' src={qr} alt=''></img>
     <a href='https://rzp.io/l/XYr6bwyR'><button className='scan'>scan to pay</button></a>
    
     </>
