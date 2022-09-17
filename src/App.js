cba4ee866b5891fc67a396d370f70a6b74762a74
shivam agarwal <shivamagarwaloff@gmail.com>
2022-09-17 15:17:10 +0530

    s

--- src/App.js
+++ src/App.js
@@ -1,50 +1,14 @@
-import React from 'react'; 
-import Form from './components/Common/Form'
-import { app } from './firebase-config';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
-import { useState } from 'react';
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Homepage from './homepage';
 function App() {
-  const [email, setEmail] = useState('');
-const [password, setPassword] = useState('')
-
-const handleAction = (id) => {
-  console.log(id);
-  const authentication = getAuth();
-  if (id === 2) {
-    createUserWithEmailAndPassword(authentication, email, password)
-      .then((response) => {
-        console.log(response)
-    })
- }
-  console.log("done")
-}
-
-
   return (
-    <BrowserRouter>
-      <div className="App">
-        <>
-          <Routes>
-            <Route path='/login' element={<Form title="Login" 
-            setEmail={setEmail}
-            setPassword={setPassword}/>} 
-            handleAction={() => handleAction(1)}/>
-            <Route path='/register' element={<Form title="Register" 
-            setEmail={setEmail}
-            setPassword={setPassword}/>} 
-            handleAction={() => handleAction(2)}
-            />
-          </Routes>
-        </>
-      </div>
-    </BrowserRouter>
+    <Router>
+      <Routes>
+      <Route path='/' element={<Homepage/>}></Route>
+      </Routes>
+
+
+    </Router>  
   );
 }
 
---
+++ src/homepage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./homepage.css"
+
+
+
+
+function Homepage() {
+
+  return (
+    <>
+    <h2 className='title'> Scan And Pay</h2>
+    <a href='https://rzp.io/l/XYr6bwyR'><button className='scan'>scan to pay</button></a>
+   
+    </>
+  )
+}
+
+export default Homepage
