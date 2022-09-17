ccd10bd35d920bec1f0cbce29490181c56af92a1
shivam agarwal <shivamagarwaloff@gmail.com>
2022-09-17 13:41:03 +0530

    s

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; 
+import Form from './components/Common/Form'
+import { app } from './firebase-config';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
+import { useState } from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
 
 function App() {
+  const [email, setEmail] = useState('');
+const [password, setPassword] = useState('')
+
+const handleAction = (id) => {
+  console.log(id);
+  const authentication = getAuth();
+  if (id === 2) {
+    createUserWithEmailAndPassword(authentication, email, password)
+      .then((response) => {
+        console.log(response)
+    })
+ }
+  console.log("done")
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <>
+          <Routes>
+            <Route path='/login' element={<Form title="Login" 
+            setEmail={setEmail}
+            setPassword={setPassword}/>} 
+            handleAction={() => handleAction(1)}/>
+            <Route path='/register' element={<Form title="Register" 
+            setEmail={setEmail}
+            setPassword={setPassword}/>} 
+            handleAction={() => handleAction(2)}
+            />
+          </Routes>
+        </>
+      </div>
+    </BrowserRouter>
   );
 }
 
